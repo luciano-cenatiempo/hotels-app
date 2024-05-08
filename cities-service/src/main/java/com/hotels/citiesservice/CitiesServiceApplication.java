953a1b45c79c672510fb41e932a9a4524077d2eb
Luciano Cenatiempo <luciano.cenatiempo.dev@gmail.com>
2024-05-08 10:35:01 -0300

    upload cities-service

---
+++ cities-service/src/main/java/com/hotels/citiesservice/CitiesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hotels.citiesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesServiceApplication.class, args);
+	}
+
+}
